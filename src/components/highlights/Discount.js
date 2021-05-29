682a42137fff11bc55206e492f9a45a3755fc6d3
Amir <amirhatam136@gmail.com>
2021-05-29 20:37:11 +0200

    test debuging

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,32 +1,50 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import { Fade, Slide } from "react-reveal";
 import MyButtons from "../utils/MyButtons";
 
 function Discount() {
-  const [isInitial, setIsInitial] = useState(true);
-  const [discountStart, setDiscountStart] = useState(0);
-  const discountEnd = 30;
+  // const [isInitial, setIsInitial] = useState(true);
+  // const [discountStart, setDiscountStart] = useState(0);
+  // const discountEnd = 30;
 
-  const percentage = () => {
-    setIsInitial(false);
-    if (discountStart < discountEnd) {
-      setDiscountStart((prevState) => prevState + 1);
-    }
-  };
+//   const percentage = () => {
+//     setIsInitial(false);
+//     if (discountStart < discountEnd) {
+//       setDiscountStart((prevState) => prevState + 1);
+//     }
+//   };
 
-  useEffect(() => {
+//   // useEffect(() => {
     
-    if (!isInitial) {
-      setTimeout(() => {
-        percentage();
-      }, 30);
-    }
-  }, [discountStart],[]);
+//   //   if (!isInitial) {
+//   //     setTimeout(() => {
+//   //       percentage();
+//   //     }, 30);
+//   //   }
+//   // }, [discountStart],[]);
 
+
+  
+
+//   useEffect(() => {
+//     const fetchBusinesses = () => {
+//         if (!isInitial) {
+//         setTimeout(() => {
+//           percentage();
+//         }, 30);
+//       }
+//   };
+//   fetchBusinesses();
+// }, [discountStart]);
+
+ 
+
+  
   return (
     <div className="container-discount">
       <div className="container">
-        <Fade onReveal={percentage} style={{ display: "inline-block" }}>
+        {/* <Fade onReveal={percentage} style={{ display: "inline-block" }}> */}
+        <Fade  style={{ display: "inline-block" }}>
           <Slide left>
             <div className="discount_description ">
               <h3> Buy before September</h3>
@@ -44,7 +62,8 @@ function Discount() {
             </div>
           </Slide>
           <div className="discount_percentage fontF">
-            <span>{discountStart}% </span>
+            {/* <span>{discountStart}% </span> */}
+            <span>30% </span>
             <span> Off</span>
           </div>
         </Fade>
